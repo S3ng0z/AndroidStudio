734107ae0eba0c9b611e85efa61ba43f654c92b6
s3ng0z <gomezs.jandres@outlook.com>
2020-02-10 18:37:10 +0100

    V-2.0 Toast (Texto flotante)

--- Dice_Roller/app/src/main/java/com/s3ng0z/dice_roller/MainActivity.kt
+++ Dice_Roller/app/src/main/java/com/s3ng0z/dice_roller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.s3ng0z.dice_roller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         //rollButton.text = "Let's roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
