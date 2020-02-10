5f54ec785066d835690e22507632fbfbe5689c3d
s3ng0z <gomezs.jandres@outlook.com>
2020-02-10 16:52:02 +0100

    Version 1.0 (Corregir)

---
+++ Dice_Roller/app/src/main/java/com/s3ng0z/dice_roller/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.s3ng0z.dice_roller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    a
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
