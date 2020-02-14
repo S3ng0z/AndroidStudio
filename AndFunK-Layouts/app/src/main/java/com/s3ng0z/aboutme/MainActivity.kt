9f724d4f16b357777e58d3404514a17e9b67efd6
s3ng0z <gomezs.jandres@outlook.com>
2020-02-14 17:17:40 +0100

    Lesson 2 Concept 13 Exercise EditText, Done Button.

--- AndFunK-Layouts/app/src/main/java/com/s3ng0z/aboutme/MainActivity.kt
+++ AndFunK-Layouts/app/src/main/java/com/s3ng0z/aboutme/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.s3ng0z.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+        val editText = findViewById<EditText>(R.id.nickname)
+        val textView = findViewById<TextView>(R.id.nickname_text)
+
+        textView.text= editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        textView.visibility = View.VISIBLE
+
+        //Hide the keyboard
+        val inn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inn.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
